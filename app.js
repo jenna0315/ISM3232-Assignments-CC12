6d076556d3e4b9f15963712c8ca2f77bc74da935
Your Name <you@example.com>
2024-10-27 22:36:29 -0400

    Task 2: Add canvas setup and event handling logic

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ let height = canvas.height;
 //enable color picker
 let colorInput = document.getElementById('color');
 
+//Task 2: Add canvas setup and event handling logic
+let drawing = false;
+let startX, startY;
+
+function draw(x, y) {
+    ctx.strokeStyle = colorInput.value;
+    ctx.lineWidth = 2;
+const tool = document.querySelector('input[name="tool"]:checked').value;
+
+if (tool === 'line') {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+} else if (tool === 'rectangle') {
+    ctx.beginPath();
+    ctx.rect(startX, startY, x - startX, y - startY);
+    ctx.stroke();
+} else if (tool === 'circle') {
+    ctx.beginPath();
+    ctx.arc(startX, startY, 10, 0, Math.PI*2,false);
+    ctx.stroke();
+}}
