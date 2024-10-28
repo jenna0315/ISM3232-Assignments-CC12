0ed089e3757750643971dbfe23dff168e75834d1
Your Name <you@example.com>
2024-10-27 22:18:03 -0400

    Task 1: Set up Canvas and HTML structure

---
+++ app.js
@@ -0,0 +1,10 @@
+//Task 1: Set up Canvas and HTML structure
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+//enable color picker
+let colorInput = document.getElementById('color');
+
